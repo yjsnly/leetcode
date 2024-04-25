fd5ff609e017ccf4998c3accb097a560ac1b1e7b
luyun <luyun@luyundeMacBook-Pro.local>
2024-04-25 14:49:40 +0800

    python: 216

---
+++ 216/solution.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        ans, stack = [], []
+        def dfs(i: int, _sum: int):
+            if len(stack) == k and _sum == n:
+                ans.append(stack.copy())
+            elif len(stack) < k and _sum < n and k - len(stack) <= 10 - i:
+                for j in range(i, 10):
+                    stack.append(j)
+                    dfs(j + 1, _sum + j)
+                    stack.pop()
+        dfs(1, 0)
+        return ans
+
+
+if __name__ == '__main__':
+    print(Solution().combinationSum3(3, 7))
